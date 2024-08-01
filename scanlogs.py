061d979dd65a08d220348f79e5ab8118a66607c4
r3quie <r3quie@gmail.com>
2024-08-01 13:30:05 +0200

    making it ugly

--- scanlogs.py
+++ scanlogs.py
@@ -2,12 +2,9 @@ def scanLogs(file):
     abc = "abcdefghijklmnopqrstuvwxyz"
     with open(file, "r") as f:
         lines = f.readlines()
-        for line in lines:
-            if "?id=" in line:
-                yield "".join([abc[(abc.find(c)+13)%26] for c in line.split("?id=")[1].split(" ")[0]])
+        return ["".join([abc[(abc.find(c)+13)%26] for c in line.split("?id=")[1].split(" ")[0]]) for line in lines if "?id=" in line]
 
 if __name__ == "__main__":
     path = input("Enter file name or path:\n")
     print("Scanning logs...\n----------------")
-    for id in scanLogs(path):
-        print(id)
+    print(scanLogs(path))
